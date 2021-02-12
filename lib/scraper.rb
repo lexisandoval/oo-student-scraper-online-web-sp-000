69934d4228de82225eba7d9343ec55733a24a071
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-12 18:00:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,15 +9,15 @@ class Scraper
   def self.scrape_index_page(index_url)
 
     doc = Nokogiri::HTML(open(index_url))
-    # doc.css(".student-card").each do |card|
-    #   student = Student.new
-    #   student.name = card.css(".student-name").text
-    #   student.location = card.css(".student-location").text
-    #   student.profile_url = card.css("a").text
-    # end
+    doc.css(".student-card").each do |card|
+      student = Student.new
+      student.name = card.css(".student-name").text
+      student.location = card.css(".student-location").text
+      student.profile_url = card.css("a").text
+    end
 
 
-  #  binding.pry
+   #binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
