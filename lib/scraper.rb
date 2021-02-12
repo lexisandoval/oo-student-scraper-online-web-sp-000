0d07e1ffa1cba685bfaaf0d8833b071a105a2e4c
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-12 18:18:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,37 +1,42 @@
 require 'open-uri'
 require 'pry'
-require 'nokogiri'
 
 class Scraper
 
-  attr_accessor :name, :location, :profile_url
+  def self.scrape_index_page(index_url) 
+    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
+    students = []
 
-  def self.scrape_index_page(index_url)
-
-    doc = Nokogiri::HTML(open(index_url))
-    doc.css(".student-card").each do |card|
-      student = Student.new
-      student.name = card.css(".student-name").text
-      student.location = card.css(".student-location").text
-      student.profile_url = card.css("a").text
-    end
-
-
-   #binding.pry
+      doc.css(".student-card").each do |student|
+        students_info = {}
+        students_info[:name] = student.css("h4.student-name").text
+        students_info[:location] = student.css("p.student-location").text
+        students_info[:profile_url] = student.css("a").attribute("href").value
+        students << students_info
+      end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
+    student_profile = {}
+    html = open(profile_url)
+    profile = Nokogiri::HTML(html)
 
-    doc = Nokogiri::HTML(open(profile_url))
-    doc.css(".student-card").each do |card|
-      student = Student.new
-      student.name = card.css(".student-name").text
-      student.location = card.css(".student-location").text
-      student.profile_url = card.css("a").text
+    profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
+      if social.attribute("href").value.include?("twitter")
+        student_profile[:twitter] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("linkedin")
+        student_profile[:linkedin] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("github")
+        student_profile[:github] = social.attribute("href").value
+      else
+        student_profile[:blog] = social.attribute("href").value
+      end
     end
 
+    student_profile[:profile_quote] = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+    student_profile[:bio] = profile.css("div.main-wrapper.profile .description-holder p").text
+
+    student_profile
   end
-
 end
-
-Scraper.scrape_index_page("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
