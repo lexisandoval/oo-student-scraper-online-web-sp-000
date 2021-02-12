0817e057abaf60d75e2e976bddc3999b3dc98a4e
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-12 18:46:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,7 +33,7 @@ class Scraper
       end
     end
 
-    student_profile[:profile_quote] = doc.css(".main-wrapper.profile .vitals-text-container .profile-quote").text
+    student_profile[:profile_quote] = doc.css(".profile-quote").text
     student_profile[:bio] = doc.css(".main-wrapper.profile .description-holder p").text
 
     student_profile
